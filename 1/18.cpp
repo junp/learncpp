a3d9c309b6278d694c5780aed7093d19c6006cca
junp <junpmail@163.com>
2014-09-02 07:55:44 +0800

    update

---
+++ 1/18.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  18.cpp
+ *
+ *    Description:  18.cpp
+ *
+ *        Version:  1.0
+ *        Created:  2014/09/02 07时35分13秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <iostream>
+
+int main(){
+	unsigned char ch = 336;
+	//std::cout << (int)ch << std::endl;
+	return 0;
+}
