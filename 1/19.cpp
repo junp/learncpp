871bbe1eb2681a3046cb2cf68531cf516c0e2572
junp <junpmail@163.com>
2014-09-02 08:00:14 +0800

    update

---
+++ 1/19.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  19.cpp
+ *
+ *    Description:  19.cpp	
+ *
+ *        Version:  1.0
+ *        Created:  2014/09/02 07时58分34秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <iostream>
+
+int main(){
+	std::cout << "a multi-line "
+		"string literal "
+		"using concatenation"
+		<< std::endl;
+	return 0;
+}
